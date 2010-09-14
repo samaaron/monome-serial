fcaccf502a43dcf307cc633009b050f3bed37a6f
Sam Aaron <samaaron@gmail.com>
2010-09-14 15:52:31 +0100

    add scheduler to try and increase the timeliness of monome events

---
+++ src/monome_serial/monome_at.clj
@@ -0,0 +1,16 @@
+(ns monome-serial.monome-at
+  (:use monome-serial.monome)
+  (:use monome-serial.at-at))
+
+(defn led-on-at
+  ([m time coords] (apply led-on-at m time coords))
+  ([m time x y]
+     (let [msg (protocol/led-on-mesg x y)]
+       (apply-at send-bytes time m msg))))
+
+(defn led-off-at
+  ([m time coords] (apply led-off-at m time coords))
+  ([m time x y]
+     (let [msg (protocol/led-off-mesg x y)]
+       (apply-at send-bytes time m msg))))
+
