dbb2393f5ea63c8cfe0f6fb1583ef94db13e8bda
Sam Aaron <samaaron@gmail.com>
2010-09-17 13:34:56 +0100

    fix bin-list->int

--- src/monome_serial/led_util.clj
+++ src/monome_serial/led_util.clj
@@ -2,11 +2,13 @@
 
 (defn bin-list->int
   [a b c d e f g h]
-  (bit-or (bit-shift-left a 1)
-          (bit-or (bit-shift-left b 2)
-                  (bit-or (bit-shift-left c 3)
-                          (bit-or (bit-shift-left d 4)
-                                  (bit-or (bit-shift-left e 5)
-                                          (bit-or (bit-shift-left f 6)
-                                                  (bit-or (bit-shift-left g 7)
-                                                          h))))))))
+  (bit-or (bit-shift-left h 7)
+          (bit-or (bit-shift-left g 6)
+                  (bit-or (bit-shift-left f 5)
+                          (bit-or (bit-shift-left e 4)
+                                  (bit-or (bit-shift-left d 3)
+                                          (bit-or (bit-shift-left c 2)
+                                                  (bit-or (bit-shift-left b 1)
+                                                          a))))))))
+
+
