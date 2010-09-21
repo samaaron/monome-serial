ce73a277ed10023ea480294aa858df01bf81d5eb
Sam Aaron <samaaron@gmail.com>
2010-09-21 17:38:40 +0100

    fix typo and remove unecessary dependency

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(def project org.clojars.samaaron/monome-serial "0.1.3"
+(defproject org.clojars.samaaron/monome-serial "0.1.3"
   :description "An interface to the monome (http://monome.org)"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [byte-spec "0.1"]]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[native-deps "1.0.4"]]
   :native-dependencies [[org.clojars.samaaron/rxtx "2.2.0"]])
 
