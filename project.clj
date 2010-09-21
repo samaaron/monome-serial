0540423dab8feb02cdf3d3d2c234619bab2d429c
Sam Aaron <samaaron@gmail.com>
2010-09-21 17:34:47 +0100

    update to new rxtx clojar (now includes native bins for more platforms)

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject org.clojars.samaaron/monome-serial "0.1.1"
+(def project org.clojars.samaaron/monome-serial "0.1.3"
   :description "An interface to the monome (http://monome.org)"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [byte-spec "0.1"]]
   :dev-dependencies [[native-deps "1.0.4"]]
-  :native-dependencies [[org.clojars.samaaron/rxtx-macosx-native-deps "2.2"]])
+  :native-dependencies [[org.clojars.samaaron/rxtx "2.2.0"]])
 
