35e95e43098fdad39326602edb6f36aae897a28a
Sam Aaron <samaaron@gmail.com>
2011-09-24 00:14:08 +0200

    Depend on Clojure 1.3.0 final

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject monome-serial "0.3.0"
+(defproject monome-serial "0.3.1"
   :description "An interface to the monome (http://monome.org)"
-  :dependencies [[org.clojure/clojure "1.3.0-RC0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/core.incubator "0.1.0"]
-                 [serial-port "1.1.0"]])
+                 [serial-port "1.1.1"]])
