ad24461b8061d634e7d8951e8bd925115c44846c
Sam Aaron <samaaron@gmail.com>
2012-03-23 12:26:37 +0000

    update to use serial-port 1.1.2 for lein 2.x compatibility

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject monome-serial "0.3.1"
+(defproject monome-serial "0.3.2"
   :description "An interface to the monome (http://monome.org)"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/core.incubator "0.1.0"]
-                 [serial-port "1.1.1"]])
+                 [serial-port "1.1.2"]])
